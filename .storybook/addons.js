d3caa04e2360e0c3b39b147119a218fc811c47e7
Sergey Gavshin <inzephirum@gmail.com>
2020-05-12 13:07:50 +0700

    chore(storybook): добавить emotion для примеров

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,7 +1,6 @@
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-actions/register';
-import '@storybook/addon-storysource/register';
 import 'storybook-addon-performance/register';
 import 'storybook-addon-themes/register';
 import '@storybook/addon-a11y/register';
