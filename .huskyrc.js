b9c6c0228fe4b76e862be4df492693a2a9712325
Oleg Denisov <c1n1kk@gmail.com>
2020-05-26 17:57:35 +0300

    chore(git): изменить конфиг husky

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,10 +2,11 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': 'lint-staged',
+    // 'pre-commit': 'lint-staged',
+
     'prepare-commit-msg': tasks([
-      'exec < /dev/tty',
-      'git cz --hook || true',
+      // 'exec < /dev/tty',
+      // 'git cz --hook || true',
       'node git-commit-message.js',
     ]),
     'pre-push': 'yarn test',
