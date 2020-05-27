b1150758f429ff668529ee0dd04a0b937b6b2eee
Oleg Denisov <c1n1kk@gmail.com>
2020-05-27 14:45:34 +0300

    chore(husky): обновить конфиг
    Jira issue: VEGA-114

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,13 +2,8 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    // 'pre-commit': 'lint-staged',
-
-    'prepare-commit-msg': tasks([
-      // 'exec < /dev/tty',
-      // 'git cz --hook || true',
-      'node git-commit-message.js',
-    ]),
+    'pre-commit': 'lint-staged',
+    'prepare-commit-msg': tasks(['node git-commit-message.js']),
     'pre-push': 'yarn test',
   },
 };
