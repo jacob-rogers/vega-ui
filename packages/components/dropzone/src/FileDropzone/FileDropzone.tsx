5f22fb632bb039bd15bbdbb8a1eaa5c4a44a6671
hitmanet <nathan200033@gmail.com>
2020-05-29 14:04:48 +0300

    feat(Dropzone): добавление FileDropzoneFullscreen
    Jira issue: VEGA-72

--- packages/components/dropzone/src/FileDropzone/FileDropzone.tsx
+++ packages/components/dropzone/src/FileDropzone/FileDropzone.tsx
@@ -5,18 +5,20 @@ import { Dropzone } from '../Dropzone';
 import { FileDropzoneInput } from './FileDropzoneInput';
 import { useFileDropzoneProvider } from './FileDropzoneProvider';
 
-type FileDropzone<T> = React.FC<T> & {
-  Fullscreen: typeof FileDropzoneFullscreen;
-  Input: typeof FileDropzoneInput;
-};
-
 type FileDropzoneProps = {
   children: React.ReactNode;
   className?: string;
+  show?: boolean;
+  fullscreen?: boolean;
+};
+
+type FileDropzone<T> = React.FC<T> & {
+  Fullscreen: React.FC<FileDropzoneProps>;
+  Input: typeof FileDropzoneInput;
 };
 
 export const FileDropzone: FileDropzone<FileDropzoneProps> = (props) => {
-  const { children, className } = props;
+  const { children, className, show = true, fullscreen = false } = props;
   const {
     handleDragEnter,
     handleDragLeave,
@@ -24,16 +26,26 @@ export const FileDropzone: FileDropzone<FileDropzoneProps> = (props) => {
     handleLoad,
   } = useFileDropzoneProvider();
 
-  const dropzoneProps = {
+  const dropzoneProps: React.ComponentProps<typeof Dropzone> = {
     className,
     children,
     onDragLeave: handleDragLeave,
     onDragOver: handleDragOver,
     onDragEnter: handleDragEnter,
     onDrop: handleLoad,
+    show,
+    fullscreen,
   };
 
   return <Dropzone {...dropzoneProps} />;
 };
+
+const FileDropzoneFullscreen: React.FC<FileDropzoneProps> = (props) => {
+  const { fullscreenVisible } = useFileDropzoneProvider();
+
+  return <FileDropzone show={fullscreenVisible} fullscreen {...props} />;
+};
+
+FileDropzone.Fullscreen = FileDropzoneFullscreen;
 
 FileDropzone.Input = FileDropzoneInput;
