88431e664b9a448b5abb72ba685e80b3a08d2e82
hitmanet <nathan200033@gmail.com>
2020-06-01 16:56:01 +0300

    test(hooks): фикс тестов для хуков
    Jira issue: VEGA-61

--- packages/hooks/src/use-mount/use-mount.ts
+++ packages/hooks/src/use-mount/use-mount.ts
@@ -14,7 +14,6 @@ export function useMount(fn: Function = (): void => {}): React.RefObject<boolean
       }
       isMountedRef.current = false;
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return isMountedRef;
 }
