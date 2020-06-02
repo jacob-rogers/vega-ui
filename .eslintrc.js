e45f26255da7f06081354c62f21f89988d76c8bf
hitmanet <nathan200033@gmail.com>
2020-06-02 15:39:57 +0300

    fix(eslint): фикс конфига
    Jira issue: VEGA-136

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,37 +2,4 @@ module.exports = {
   extends: [
     require.resolve('@gpn-prototypes/frontend-configs/eslintrc')
   ],
-  overrides: [
-    {
-      files: [
-        "./src/**/*.ts"
-      ],
-      rules: {
-        "ordered-imports": "off",
-        "no-restricted-imports": [
-          "error",
-          {
-            patterns: [
-              "@/*",
-              "**/components/**/*",
-              "**/contexts/**/*",
-              "**/dashboard/**/*",
-              "**/ui/**/*",
-              "**/utils/**/*",
-              "**/widget-helpers/**/*",
-              "**/widgets/**/*"
-            ]
-          }
-        ]
-      }
-    },
-    {
-      files: [
-        "./src/**/index.stories.tsx"
-      ],
-      rules: {
-        "import/no-default-export": ["off"]
-      }
-    }
-  ],
 };
