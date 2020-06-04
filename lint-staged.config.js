ba7893bed2b5ff675b0287004580bdbaaeabcbfc
hitmanet <nathan200033@gmail.com>
2020-06-04 13:40:25 +0300

    feat(remark): remark в lint-staged
    Jira issue: VEGA-136

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,1 +1,5 @@
-module.exports = require('@gpn-prototypes/frontend-configs/git/lint-staged.config');
+module.exports = {
+  // eslint-disable-next-line global-require
+  ...require('@gpn-prototypes/frontend-configs/git/lint-staged.config'),
+  '*.{md}': ['yarn lint:md', 'git add'],
+};
