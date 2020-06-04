334e3de808cb005352ac0aa2bcb814bbd72d861e
hitmanet <nathan200033@gmail.com>
2020-06-04 13:42:46 +0300

    fix(babel): удалил лишний плагин
    Jira issue: VEGA-136

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   // eslint-disable-next-line global-require
   ...require('@gpn-prototypes/frontend-configs/babel.config'),
-  plugins: [
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        regenerator: true,
-      },
-    ],
-  ],
 };
