a0d606967985855a7b85aa13be82177c9e874990
Oleg <c1n1kk@gmail.com>
2020-06-16 14:46:25 +0300

    VEGA-171: Добавить экспорт лого (#78)
    * fix(logo): добавить экспорт в корневой пакет
    
    Jira issue: VEGA-171
    
    * fix(logo): пофиксить package.json
    
    Jira issue: VEGA-171
    
    * fix(logo): обновить конфиг сборки
    
    Jira issue: VEGA-171
    
    * fix(logo): изменить версию пакета
    
    Jira issue: VEGA-171

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -25,3 +25,4 @@ export * from '@gpn-prototypes/vega-progress-spin';
 export * from '@gpn-prototypes/vega-attach';
 export * from '@gpn-prototypes/vega-loader';
 export * from '@gpn-prototypes/vega-carousel';
+export * from '@gpn-prototypes/vega-logo';
