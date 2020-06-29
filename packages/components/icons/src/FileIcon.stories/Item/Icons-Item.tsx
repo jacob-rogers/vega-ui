e646e20db0c7041c6bc4e78478a14a5467c40610
hitmanet <nathan200033@gmail.com>
2020-06-29 13:50:23 +0300

    docs(root): добавление доки
    Jira issue: VEGA-61
    
    refactor(root): правки после ревью
    
    Jira issue: VEGA-61

--- packages/components/icons/src/FileIcon.stories/Item/Icons-Item.tsx
+++ packages/components/icons/src/FileIcon.stories/Item/Icons-Item.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { FileIconProps } from '@gpn-design/uikit/FileIcon';
 
-export type IconPropssItem = {
+export type IconItemProps = {
   icon: React.FC<FileIconProps>;
   name: string;
 } & FileIconProps;
 
-export const IconsItem: React.FC<IconPropssItem> = ({ icon, name, size }) => {
+export const IconsItem: React.FC<IconItemProps> = ({ icon, name, size }) => {
   const Icon = icon;
   return (
     <div className="tpl-grid__fraction text text_align_center">
