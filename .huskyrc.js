9b0b8afc4e34c05250af5232523fef94766bd38e
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-07-01 16:14:05 +0300

    chore(husky): изменен путь до файла с конфигом
    Jira issue: VEGA-46

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ const tasks = (arr) => arr.join(' && ');
 
 module.exports = {
   hooks: {
-    'pre-commit': 'lint-staged -c ./node_modules/@gpn-prototypes/frontend-configs/git/lint-staged.config.js',
+    'pre-commit': 'lint-staged -c ./lint-staged.config.js',
     'prepare-commit-msg': tasks([
       'exec < /dev/tty',
       'git cz --hook || true',
