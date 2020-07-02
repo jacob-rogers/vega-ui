dbbd54d0d5932f467b143fc5a9557c972773d48a
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-07-02 18:45:59 +0300

    chore(eslint): предупреждение про any сделано ошибкой
    Jira issue: VEGA-46

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: [require.resolve('@gpn-prototypes/frontend-configs/eslintrc')],
   rules: {
     '@typescript-eslint/no-empty-function': 'off',
-  },
+    '@typescript-eslint/no-explicit-any': 'error',
+  }
 };
