ee31af91059301c60cfa2543cad5d14756e74c03
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-07-02 14:56:34 +0300

    test(Layout): todo для тестов
    Jira issue: VEGA-46

--- packages/components/layout/src/Layout.test.tsx
+++ packages/components/layout/src/Layout.test.tsx
@@ -18,4 +18,33 @@ describe('Layout', () => {
   test('рендерится без ошибок', () => {
     renderComponent();
   });
+
+  test('корректно определяются resizeDirection у Layout.Window', () => {
+    render(
+      <Layout columns={[60, 40]}>
+        <Layout.Window />
+      </Layout>,
+    );
+  });
+
+  test.todo('получаем варнинг, если в детей передан не Layout.Window');
+  test.todo('игнорируются rows, если передан один ребенок');
+  test.todo('игнорируются columns, если передан один ребенок');
+  test.todo('нельзя передать больше одного ребенка, и не указать rows или columns');
+  test.todo('нельзя одновременно передать rows и columns');
+  test.todo('сумма rows не больше 100');
+  test.todo('сумма columns не больше 100');
+});
+
+describe('Layout.Window', () => {
+  test.todo('получаем варнинг, если не является потомком Layout');
+  test.todo('получаем варнинг, если пытаемся передать контент без Layout.Body или Layout.Header');
+});
+
+describe('Layout.Body', () => {
+  test.todo('получаем варнинг, если не является потомком Layout.Window');
+});
+
+describe('Layout.Header', () => {
+  test.todo('получаем варнинг, если не является потомком Layout.Window');
 });
