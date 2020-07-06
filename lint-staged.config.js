6612fd3672b5acca945c0c397ee57b177fd35c28
Roman Gurinovich <gurinovich.rv@gmail.com>
2020-07-06 14:48:25 +0300

    chore(lint-staged): исправить lint-staged для md
    Jira issue: VEGA-199

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // eslint-disable-next-line global-require
   ...require('@gpn-prototypes/frontend-configs/git/lint-staged.config'),
-  '*.{md}': ['yarn lint:md', 'git add'],
+  '*.md': ['yarn lint:md:fix'],
 };
