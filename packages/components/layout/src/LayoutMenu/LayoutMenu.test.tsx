9765e0c09ce8aeacefe70364e821725c26e7a6e9
hitmanet <nathan200033@gmail.com>
2020-07-07 12:59:04 +0300

    feat(layout-menu): изменение апи для порталов и тесты
    Jira issue: VEGA-129

--- packages/components/layout/src/LayoutMenu/LayoutMenu.test.tsx
+++ packages/components/layout/src/LayoutMenu/LayoutMenu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, RenderResult, screen } from '@testing-library/react';
+import { act, fireEvent, render, RenderResult, screen } from '@testing-library/react';
 
 import { LayoutMenu, LayoutMenuProps } from './LayoutMenu';
 
@@ -15,10 +15,62 @@ function renderComponent(
   return render(<LayoutMenu {...props} items={items} />);
 }
 
+function findTrigger(): Promise<HTMLElement> {
+  return screen.findByLabelText('Триггер для меню layout');
+}
+
+async function clickByTrigger(): Promise<void> {
+  const trigger = await findTrigger();
+
+  act(() => {
+    fireEvent.click(trigger);
+  });
+}
+
 describe('LayoutMenu', () => {
   it('рендерится без ошибок', () => {
     expect(renderComponent).not.toThrow();
   });
 
-  it('рендерится дропдаун', async () => {});
+  it('рендерится триггер', async () => {
+    renderComponent();
+
+    const trigger = await findTrigger();
+
+    expect(trigger).toBeInTheDocument();
+  });
+
+  it('рендерится элементы меню', async () => {
+    renderComponent();
+
+    await clickByTrigger();
+
+    const menuItem = await screen.getByLabelText('test');
+
+    expect(menuItem).toBeInTheDocument();
+  });
+
+  it('вызывается onChange по клику на элемент', async () => {
+    const onChange = jest.fn();
+
+    renderComponent({ onChange });
+
+    await clickByTrigger();
+
+    const menuItem = await screen.getByLabelText('test');
+
+    fireEvent.click(menuItem);
+
+    expect(onChange).toBeCalled();
+  });
+
+  it('выставляется активный элемент', async () => {
+    renderComponent({ activeValue: 'test', onChange: jest.fn() });
+
+    await clickByTrigger();
+
+    const menuItem = await screen.getByLabelText('test');
+
+    expect(menuItem.classList.contains('VegaNavigationList__Item_active')).toBe(true);
+  });
 });
