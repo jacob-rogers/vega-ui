a2c783f94af75cb8917a483df059fed0700d9606
hitmanet <nathan200033@gmail.com>
2020-07-15 13:17:47 +0300

    feat(layout): добавление верстки для сплита
    Jira issue: VEGA-125

--- packages/components/layout/src/Layout.test.tsx
+++ packages/components/layout/src/Layout.test.tsx
@@ -1,50 +1,112 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { Root } from '@gpn-prototypes/vega-root';
+import { act, fireEvent, render, RenderResult, screen } from '@testing-library/react';
 
 import { Layout, LayoutProps } from './Layout';
 
-function renderComponent(props?: LayoutProps): RenderResult {
+function renderComponent(props: LayoutProps = { onChange: jest.fn() }): RenderResult {
   return render(
-    <Layout {...props}>
-      <Layout.Window>
-        <Layout.Header />
-        <Layout.Body />
-      </Layout.Window>
-    </Layout>,
+    <Root>
+      <Layout {...props} />
+    </Root>,
   );
+}
+
+const labels = [
+  'Добавить панель справа',
+  'Добавить панель слева',
+  'Добавить панель сверху',
+  'Добавить панель снизу',
+];
+
+function findOptionsTrigger(): Promise<HTMLElement> {
+  return screen.findByLabelText('Открыть опции панели');
+}
+
+async function clickByTrigger(): Promise<void> {
+  const trigger = await findOptionsTrigger();
+
+  act(() => {
+    fireEvent.click(trigger);
+  });
+}
+
+async function clickByOption(label: string): Promise<void> {
+  await clickByTrigger();
+
+  const option = await screen.getByLabelText(label);
+
+  act(() => {
+    fireEvent.click(option);
+  });
 }
 
 describe('Layout', () => {
   test('рендерится без ошибок', () => {
-    renderComponent();
+    expect(renderComponent).not.toThrow();
   });
 
-  test('корректно определяются resizeDirection у Layout.Window', () => {
-    render(
-      <Layout sizes={[60, 40]}>
-        <Layout.Window />
-      </Layout>,
-    );
+  describe('Splitting', () => {
+    test.each(labels)('рендерит опцию "%s"', async (label) => {
+      renderComponent();
+
+      await clickByTrigger();
+
+      const option = await screen.findByLabelText(label);
+
+      expect(option).toBeInTheDocument();
+    });
+
+    test('вызывается onChange', async () => {
+      const onChange = jest.fn();
+
+      renderComponent({ onChange });
+
+      await clickByOption(labels[0]);
+
+      expect(onChange).toBeCalledWith({ idx: 0, type: 'split' });
+    });
+
+    test('опции закрываются после клика на элемент', async () => {
+      const { container } = renderComponent();
+
+      await clickByOption(labels[0]);
+
+      expect(container.querySelector('.VegaLayout__MenuTrigger')).toBeInTheDocument();
+
+      expect(container.querySelector('.VegaLayout__List')).not.toBeInTheDocument();
+    });
+
+    test('создается новое окно в панели', async () => {
+      const { container } = renderComponent();
+
+      expect(container.querySelectorAll('.VegaLayout__Window').length).toBe(1);
+
+      await clickByOption(labels[0]);
+
+      expect(container.querySelectorAll('.VegaLayout__Window').length).toBe(2);
+    });
+
+    test('панель закрывается', async () => {
+      const { container } = renderComponent();
+
+      await clickByOption(labels[0]);
+
+      expect(container.querySelectorAll('.VegaLayout__Window').length).toBe(2);
+
+      const triggers = await screen.findAllByLabelText('Открыть опции панели');
+
+      act(() => {
+        fireEvent.click(triggers[0]);
+      });
+
+      const closeOption = await screen.findByLabelText('Закрыть панель');
+
+      act(() => {
+        fireEvent.click(closeOption);
+      });
+
+      expect(container.querySelectorAll('.VegaLayout__Window').length).toBe(1);
+    });
   });
-
-  test.todo('получаем варнинг, если в детей передан не Layout.Window');
-  test.todo('игнорируются rows, если передан один ребенок');
-  test.todo('игнорируются columns, если передан один ребенок');
-  test.todo('нельзя передать больше одного ребенка, и не указать rows или columns');
-  test.todo('нельзя одновременно передать rows и columns');
-  test.todo('сумма rows не больше 100');
-  test.todo('сумма columns не больше 100');
-});
-
-describe('Layout.Window', () => {
-  test.todo('получаем варнинг, если не является потомком Layout');
-  test.todo('получаем варнинг, если пытаемся передать контент без Layout.Body или Layout.Header');
-});
-
-describe('Layout.Body', () => {
-  test.todo('получаем варнинг, если не является потомком Layout.Window');
-});
-
-describe('Layout.Header', () => {
-  test.todo('получаем варнинг, если не является потомком Layout.Window');
 });
