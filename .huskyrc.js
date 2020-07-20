a3dd35369fbe3594235140e13b82aaf377248d13
Tatyana Tsvetkova <tsvetta@gmail.com>
2020-07-20 15:39:01 +0300

    chore(husky): обновлены frontend-configs
    Jira issue: VEGA-109

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,13 +1,6 @@
-const tasks = (arr) => arr.join(' && ');
-
 module.exports = {
   hooks: {
-    'pre-commit': 'yarn lint-staged -c ./lint-staged.config.js',
-    'prepare-commit-msg': tasks([
-      'exec < /dev/tty',
-      'git cz --hook || true',
-      'node ./node_modules/@gpn-prototypes/frontend-configs/git/git-commit-message.js',
-    ]),
+    ...require('@gpn-prototypes/frontend-configs/.huskyrc.js').hooks,
     'pre-push': 'yarn test',
   },
 };
