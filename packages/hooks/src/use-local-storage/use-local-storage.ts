9dad93b752e24c256217177de8a724b7310349f1
hitmanet <nathan200033@gmail.com>
2020-08-07 11:48:27 +0300

    feat(canvas): добавление сохрания позиции в ls
    Jira issue: VEGA-304

---
+++ packages/hooks/src/use-local-storage/use-local-storage.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+type API<T> = [T, (value: T) => void];
+
+export function useLocalStorage<T>(key: string, initialValue: T): API<T> {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T): void => {
+    const valueToStore = value instanceof Function ? value(storedValue) : value;
+    setStoredValue(valueToStore);
+    window.localStorage.setItem(key, JSON.stringify(valueToStore));
+  };
+
+  return [storedValue, setValue];
+}
