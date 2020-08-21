0b9561127a519b220b9123061a9a4f7849bbaff0
dmitriibo <alienshooter2008@yandex.ru>
2020-08-21 22:09:53 +0400

    refactor(components):
    Jira issue: VEGA-147

--- packages/components/tree/src/Tree.test.tsx
+++ packages/components/tree/src/Tree.test.tsx
@@ -2,14 +2,75 @@ import React from 'react';
 import { render, RenderResult } from '@testing-library/react';
 
 import { Tree } from './Tree';
-import {RootTreeProps} from "./types";
+import { RootTreeProps } from './types';
+
+const items: RootTreeProps = {
+  name: 'Усть-Енисей',
+  nodeList: [
+    {
+      name: 'Поднятие 44-23',
+      nodeList: [
+        {
+          name: 'Залежь - 78',
+          nodeList: [
+            {
+              name: 'Ловушка 100',
+              nodeList: [
+                {
+                  name: 'Еще что нибудь',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'Залежь - 79',
+          nodeList: [
+            {
+              name: 'Ловушка 101',
+            },
+          ],
+        },
+        {
+          name: 'Залежь - 56',
+        },
+        {
+          name: 'Залежь - 11',
+        },
+        {
+          name: 'Залежь - 1',
+        },
+      ],
+    },
+    {
+      name: 'Поднятие 55-100',
+      nodeList: [
+        {
+          name: 'Залежь - 78',
+        },
+        {
+          name: 'Залежь - 79',
+        },
+        {
+          name: 'Залежь - 56',
+        },
+        {
+          name: 'Залежь - 11',
+        },
+        {
+          name: 'Залежь - 1',
+        },
+      ],
+    },
+  ],
+};
 
 function renderComponent(props?: RootTreeProps): RenderResult {
-  return render(<Tree {...props} />);
+  return render(<Tree {...items} />);
 }
 
 describe('Tree', () => {
   test('рендерится без ошибок', () => {
-    renderComponent({});
+    renderComponent({ ...items });
   });
 });
--- packages/components/tree/src/types.ts
+++ packages/components/tree/src/types.ts
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
 export interface BaseNode {
-  name: string,
-  id?: string | number,
-  handleDragStart?(event: React.DragEvent) : void,
-  handleDragOver?(event: React.DragEvent) : void,
-  handleDragDrop?(event: React.DragEvent) : void,
+  name: string;
+  id?: string | number;
+  handleDragStart?(event: React.DragEvent): void;
+  handleDragOver?(event: React.DragEvent): void;
+  handleDragDrop?(event: React.DragEvent): void;
 }
 
 export interface NodeTreeType extends BaseNode {
-  nodeList?: NodeTreeType[],
-  leaf?: LeafType[] | LeafType,
-  children: React.ReactNode,
+  nodeList?: NodeTreeType[];
+  leaf?: LeafType[] | LeafType;
+  children?: React.ReactNode;
 }
 
 export interface RootTreeProps extends BaseNode {
-  nodeList: NodeTreeType[],
-  leaf?: LeafType[] | LeafType,
+  nodeList: NodeTreeType[];
+  leaf?: LeafType[] | LeafType;
 }
 
 export type LeafType = {
-  name: string,
-  handleDragStart(event: React.DragEvent) : void,
-}
+  name: string;
+  handleDragStart(event: React.DragEvent): void;
+};
