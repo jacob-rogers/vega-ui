e4a5367bb76a1101d9780a4f17b8b8bf6e32945a
hitmanet <nathan200033@gmail.com>
2020-08-21 15:27:46 +0300

    fix(canvas): фикс канваса
    Jira issue: VEGA-335

--- packages/components/canvas/src/Canvas.tsx
+++ packages/components/canvas/src/Canvas.tsx
@@ -2,12 +2,7 @@ import React, { useEffect, useMemo } from 'react';
 import { useLocalStorage, useMount } from '@gpn-prototypes/vega-hooks';
 
 import { CanvasView } from './CanvasView';
-<<<<<<< HEAD
 import { Canvas as CanvasEntity, CanvasTree, Context, FlatTree, Tree } from './entities';
-import { Position } from './types';
-=======
-import { Canvas as CanvasEntity, CanvasTree, Context, FlatTree, Node, Tree } from './entities';
->>>>>>> 42cb8e9... fix(lint): убрал лишнее
 
 import './Canvas.css';
 
