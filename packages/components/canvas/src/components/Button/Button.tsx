0014651924a4b3fc75b5d7b169154571327f770d
hitmanet <nathan200033@gmail.com>
2020-09-01 16:31:41 +0300

    feat(canvas): добавление возможности удалять элементы в канвасе
    Jira issue: VEGA-364

--- packages/components/canvas/src/components/Button/Button.tsx
+++ packages/components/canvas/src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Konva from 'konva';
 
 import { useCanvas } from '../../context';
-import { Position } from '../../types';
+import { KonvaMouseEvent, Position } from '../../types';
 import { ListItem } from '../ListItem';
 
 type ButtonProps = {
@@ -26,7 +26,10 @@ export const Button: React.FC<ButtonProps> = (props) => {
       onMouseLeave={(): void => {
         setCursor('default');
       }}
-      onClick={onClick}
+      onClick={(e: KonvaMouseEvent): void => {
+        e.cancelBubble = true;
+        onClick(e);
+      }}
       label={label}
       draggable={false}
     />
