4be5e217a79bf38658cb80f1933240e40fcb84a2
Sergey Gavshin <inzephirum@gmail.com>
2021-01-15 17:47:16 +0700

    VEGA-818: Настроить test coverage для vega-ui (#425)
    * chore(jest): исключить .d.ts файлы из отчета покрытия
    
    Jira issue: VEGA-818
    
    * chore(ci): добавить coverage workflow
    
    Jira issue: VEGA-818

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,9 @@ const config = require('@gpn-prototypes/frontend-configs/jest/jest.config');
 
 module.exports = {
   ...config({ setupFilesAfterEnv: setupTestFile }),
+  coveragePathIgnorePatterns: ['/node_modules/', '/coverage/', '/types/'],
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/*.stories.tsx', '!**/*.d.ts'],
+  coverageReporters: ['lcov', 'json-summary', 'text', 'text-summary'],
   moduleNameMapper: {
     ...pathsToModuleNameMapper(tsConfig.compilerOptions.paths, { prefix: '<rootDir>/' }),
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
