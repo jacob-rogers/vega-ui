c705d8752ce16524d2f2865823dc727f0b97166e
Oleg <c1n1kk@gmail.com>
2021-01-20 14:17:37 +0300

    Fix multicombobox (#460)
    * refactor(multicombobox): изменить тип компонента
    
    * refactor(test): изменить типы в тестах
    
    * fix(multicombobox): исправить тип

--- src/components/multi-combobox/MultiCombobox.tsx
+++ src/components/multi-combobox/MultiCombobox.tsx
@@ -4,6 +4,6 @@ import { MultiCombobox as BaseMultiCombobox } from '@consta/uikit/MultiCombobox'
 export type MultiComboboxProps = React.ComponentProps<typeof BaseMultiCombobox>;
 export type MultiComboboxType = typeof BaseMultiCombobox;
 
-export const MultiCombobox: React.FC<MultiComboboxProps> = (props) => {
+export const MultiCombobox: MultiComboboxType = (props) => {
   return <BaseMultiCombobox {...props} />;
 };
